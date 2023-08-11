03ebb2be0327db672e46bae302dca05403f44ecc
noturcmd <ridwan170604bakhri@gmail.com>
2023-08-12 02:18:58 +0700

    test 2

--- script/konversiTipeData.js
+++ script/konversiTipeData.js
@@ -1,11 +1,16 @@
 
-var angka = prompt("Masukkan angka : ");
-document.writeln("Angka yang Anda masukkan : " + angka);
-document.writeln("<br>" + "Tipe data dari inputan prompt : "+ typeof(angka));
+// var angka = prompt("Masukkan angka : ");
+// document.writeln("Angka yang Anda masukkan : " + angka);
+// document.writeln("<br>" + "Tipe data dari inputan prompt : "+ typeof(angka));
 
 
-var jumlahkanDenganLima = function(){
-    return Number(angka) + 5;
-}
+// var jumlahkanDenganLima = function(){
+//     return Number(angka) + 5;
+// }
 
-document.writeln("<br>Hasil penjumlahan dengan lima : " + jumlahkanDenganLima());
+// document.writeln("<br>Hasil penjumlahan dengan lima : " + jumlahkanDenganLima());
+
+
+var ubahLayout = document.getElementsByTagName("li");
+console.log(ubahLayout);
+ubahLayout[0].style.backgroundColor = "red";
