515c96ab305607c598d36a3d351c5f75c74750e8
noturcmd <ridwan170604bakhri@gmail.com>
2023-08-12 02:14:11 +0700

    test

---
+++ script/konversiTipeData.js
@@ -0,0 +1,11 @@
+
+var angka = prompt("Masukkan angka : ");
+document.writeln("Angka yang Anda masukkan : " + angka);
+document.writeln("<br>" + "Tipe data dari inputan prompt : "+ typeof(angka));
+
+
+var jumlahkanDenganLima = function(){
+    return Number(angka) + 5;
+}
+
+document.writeln("<br>Hasil penjumlahan dengan lima : " + jumlahkanDenganLima());
